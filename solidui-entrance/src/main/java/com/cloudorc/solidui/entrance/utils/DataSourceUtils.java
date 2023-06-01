e636e582b0aed575435c07783f41512a797498e4
Dlimeng <77954309@qq.com>
2023-06-01 22:03:40 +0800

    pom spotless

--- solidui-entrance/src/main/java/com/cloudorc/solidui/entrance/utils/DataSourceUtils.java
+++ solidui-entrance/src/main/java/com/cloudorc/solidui/entrance/utils/DataSourceUtils.java
@@ -17,27 +17,25 @@
 
 package com.cloudorc.solidui.entrance.utils;
 
-
 import com.cloudorc.solidui.common.constants.Constants;
-import com.cloudorc.solidui.common.utils.JSONUtils;
 import com.cloudorc.solidui.dao.entity.DataSource;
 import com.cloudorc.solidui.entrance.dto.DataSourceDTO;
-import com.cloudorc.solidui.entrance.exceptions.ServiceException;
 import com.cloudorc.solidui.plugin.jdbc.ConnectDTO;
 import com.cloudorc.solidui.plugin.jdbc.JdbcClient;
 import com.cloudorc.solidui.plugin.jdbc.JdbcClientFactory;
 import com.cloudorc.solidui.spi.ConstantsSPI;
-import com.fasterxml.jackson.core.type.TypeReference;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DataSourceUtils {
+
     private final static Logger logger = LoggerFactory.getLogger(DataSourceUtils.class);
     private final static Map<String, JdbcClientFactory> jdbcClientFactoryInstances = new ConcurrentHashMap<>();
     /**
@@ -53,7 +51,7 @@ public class DataSourceUtils {
             throw new IllegalArgumentException(typeName + " is not supported");
         }
         JdbcClientFactory jdbcClientFactory = jdbcClientFactoryInstances.get(typeName);
-        if(jdbcClientFactory == null){
+        if (jdbcClientFactory == null) {
             String prefix = typeName.substring(0, 1).toUpperCase() + typeName.substring(1);
             String className = String.format(ConstantsSPI.DATASOURCE_CLASSNAME, prefix);
             Class<?> clazz = classLoader.loadClass(className);
@@ -69,15 +67,16 @@ public class DataSourceUtils {
 
     }
 
-    public static JdbcClient queryJdbcClient(String typeName,DataSource dataSource) throws SQLException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public static JdbcClient queryJdbcClient(String typeName,
+                                             DataSource dataSource) throws SQLException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         JdbcClient jdbcClient = null;
         JdbcClientFactory jdbcClientFactory = DataSourceUtils.queryJdbcClientFactory(typeName);
         DataSourceDTO dataSourceDTO = new DataSourceDTO();
         dataSourceDTO.setId(dataSource.getId());
         dataSourceDTO.setParameter(dataSource.getParameter());
         ConnectDTO connectDTO = jdbcClientFactory.getConnectDTO(dataSourceDTO.getConnectParams());
-        if(connectDTO != null) {
-             jdbcClient = jdbcClientFactory.createJdbcClient(connectDTO);
+        if (connectDTO != null) {
+            jdbcClient = jdbcClientFactory.createJdbcClient(connectDTO);
         }
         return jdbcClient;
     }
