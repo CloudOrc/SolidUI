fa4584216bd6ea9f3ccd1334971dcfa6c8efea6e
dlimeng <mengyoupanshan@gmail.com>
2023-12-13 15:05:32 +0800

    flask  api

--- solidui/views/__init__.py
+++ solidui/views/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -12,13 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-# URL of the backend service of solidui-views
-solidui_url="http://127.0.0.1:12345"
-solidui_url_models="http://127.0.0.1:5110"
-solidui_url_kernel="http://127.0.0.1:5010"
-
-# solidui ip address，the http access address for solidui-web will be http://${solidui_ipaddr}:${solidui_port}
-solidui_ipaddr=127.0.0.1
-solidui_port=8099
---
+++ solidui/views/base_api.py
@@ -0,0 +1,3 @@
+
+class BaseSupersetApi(BaseSupersetApiMixin, BaseApi):
+    ...
