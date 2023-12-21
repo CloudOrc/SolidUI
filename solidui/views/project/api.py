5f3d7871280b48403a8eeb221ccca47c175fbf5b
dlimeng <mengyoupanshan@gmail.com>
2023-12-22 04:07:38 +0800

    interface userapi

--- solidui/views/project/api.py
+++ solidui/views/project/api.py
@@ -11,27 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import threading
-from concurrent.futures import ThreadPoolExecutor
 
-def thread_factory(thread_name: str, is_daemon: bool):
-    thread_id = 0
 
-    def create_thread(func):
-        nonlocal thread_id
-        thread_id += 1
-        thread_name = f'{thread_name}{thread_id}'
-        thread = threading.Thread(target=func, name=thread_name)
-        thread.setDaemon(is_daemon)
-        return thread
-
-    return create_thread
-
-def default_scheduler():
-    executor = ThreadPoolExecutor(
-        max_workers=20,
-        thread_name_prefix='SolidUI-Default-Scheduler-Thread-',
-        thread_factory=thread_factory
-    )
-    executor.set_keep_alive(5, 60)
-    return executor
